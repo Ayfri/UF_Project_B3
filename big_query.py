1b664107d2ceb61f9183b695e355e80db1efb73f
Ayfri <pierre.ayfri@gmail.com>
2024-03-28 15:26:41 +0100

    feat(query): Use dotenv for connecting to BigQuery.

--- big_query.py
+++ big_query.py
@@ -1,9 +1,15 @@
+import os
+
+import dotenv
 from google.cloud import bigquery
+from google.oauth2 import service_account
 from pprint import pprint
 
+dotenv.load_dotenv()
 
 def main() -> None:
-    client = bigquery.Client()
+    credentials = service_account.Credentials.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    client = bigquery.Client(credentials=credentials)
 
     # Perform a query.
     QUERY = (
