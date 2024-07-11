f49a2c4a727efac68710845d5bba5a1bf1d9126d
Ayfri <pierre.ayfri@gmail.com>
2024-07-11 11:20:25 +0200

    feat(styles-and-components): Add new color variables and apply them to styles, refactor nav links display in header.

--- pages/components.py
+++ pages/components.py
@@ -3,14 +3,14 @@ from dash.development.base_component import Component
 
 headers = [
 	dcc.Link('World Page', href='/'),
-	html.Br(),
 	dcc.Link('Advanced Search', href='/advanced'),
-	html.Br(),
 ]
 
 
 def header(title: str) -> Component:
 	return html.Header(children=[
 		html.H1(title),
-		*headers
+		html.Nav(children=[
+			*headers
+		])
 	])
