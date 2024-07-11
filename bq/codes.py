30c421859c150a9c47a6577a1c447df711826330
Ayfri <pierre.ayfri@gmail.com>
2024-07-11 02:54:17 +0200

    refactor(codes): Improve way of loading JSON data into pandas dataframe, minor modification of `add_event_code_names` function.

--- bq/codes.py
+++ bq/codes.py
@@ -1,12 +1,15 @@
+import json
+
 import pandas as pd
 
-event_codes = pd.read_json('data/cameo_mapping_fr.json', orient='index')
-event_codes = event_codes.rename(columns={0: 'Description'})
-event_codes = event_codes.reset_index().rename(columns={'index': 'EventCode'})
+with open('data/cameo_mapping_fr.json', 'r', encoding='utf-8') as f:
+	data = json.load(f, object_pairs_hook=lambda pairs: dict(pairs))
+
+event_codes = pd.DataFrame.from_dict(data, orient='index', columns=['Description'])
+event_codes = event_codes.reset_index().rename(columns={'index': 'EventCode', 0: 'Description'})
 event_codes['Description'] = event_codes['Description'].str.title()
 
 
 def add_event_code_names(df: pd.DataFrame) -> pd.DataFrame:
-	df['EventCode'] = df['EventCode'].astype('int64')
 	df = df.merge(event_codes, on='EventCode', how='left')
 	return df
